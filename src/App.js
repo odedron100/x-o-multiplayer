8c56affce8620499dbca6ca576e78e00cdea1a1b
odedron100 <odedron84@gmail.com>
2020-10-27 14:09:41 +0200

    Add components (game and opponents) to the app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Game from './Game';
+import Opponent from './Opponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <Switch>
+            <Route path="/opponent" component={Opponent}>
+            </Route>
+            <Route path="/" component={Game}>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  } 
 }
 
 export default App;
